a951390808e950714ede8ea5a4a70eefcee55b99
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-03-26 17:47:36 +0700

    moved everything into one dir

--- 3-6/app/main.py
+++ 3-6/app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
+from ..parser import parse
+
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
+@app.get("/parse/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+    parse('@some_channel', o='out.csv', last=['5', 'd'], views=True, content_text=True)
