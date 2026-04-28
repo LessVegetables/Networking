b70dc2fe64952d66afc257e89146d8f26ad50a92
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-04-28 15:02:58 +0700

    testing parameters/args

--- tchannels.org/app/main.py
+++ tchannels.org/app/main.py
@@ -3,6 +3,7 @@ from parser import parse
 
 # ADD channel_id PARSE
 # INT the view count
+FLAGS = ["post_id", "post_link", "author_name", "author_link", "datetime", "last_scrape_datetime", "views", "content_text", "content_img"]
 
 app = FastAPI()
 
@@ -13,10 +14,20 @@ def read_root():
 @app.get("/parse/{channel}")
 def read_item(channel: str, request: Request):
 
-    parse(f'@{channel}', o='out.csv', last=['5', 'd'], views=True, content_text=True)
 
     params = dict(request.query_params)
+
+    params_keys = list(params.keys())
+
+    for k in params_keys:
+        if k in FLAGS:
+            params[k] = True
+        else:
+            params.pop(k)
+    
     d = request.query_params.get("d")
+
+    parse(f'@{channel}', o='out.csv', last=['5', 'd'], flags=params)
 
     return {"parsed channel": channel, "params": params}
 
