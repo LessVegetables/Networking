7aedf886d5f8e23280244387b347abc772c50b3d
LessVegetables <57289239+LessVegetables@users.noreply.github.com>
2026-05-08 01:18:22 +0700

    added deploy stuff

---
+++ tchannels.org/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker network create tchan_net
+
+docker run -d \
+  --name tchannels_db \
+  --network tchan_net \
+  --restart unless-stopped \
+  -e POSTGRES_USER=admin \
+  -e POSTGRES_PASSWORD=secret \
+  -e POSTGRES_DB=tchannels \
+  -v pgdata:/var/lib/postgresql/data \
+  postgres:16
+
+docker build -t tchannels_image -f ./app/dockerfile ./app/
+
+docker run -d \
+    --network tchan_net \
+    -p 127.0.0.1:8501:80 \
+    --restart unless-stopped \
+    --name tchannels_app \
+    tchannels_image
---
+++ tchannels.org/reload_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker build -t tchannels_image -f ./app/dockerfile ./app/
+docker stop tchannels_app || true
+docker rm tchannels_app || true
+docker run -d \
+    -p 127.0.0.1:8501:80 \
+    --network tchan_net \
+    --restart unless-stopped \
+    --name tchannels_app \
+    tchannels_image
